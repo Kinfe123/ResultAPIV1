74a03c9908538a3a4af9ce7ea3272491a45babe1
Kinfe Michael Tariku <65047246+Kinfe123@users.noreply.github.com>
2022-04-04 12:43:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const fs = require('fs')
+var data = fs.readFileSync('kinfe.json')
+var result = JSON.parse(data)
+const app  = express()
+const PORT = 5000 || process.env.PORT
+app.listen((PORT ) , ()=>{
+    console.log("Server started at the PORT " , PORT )
+})
+app.use(express.static('public'));
+app.get("/" , (req , res)=>{
+    res.send("Welcome to the result API...You can explore the student result at /results page")
+})
+app.get('/results' , (req , res)=>{
+    res.send(result)
+    
+
+})
+app.get('/results/:result/' , (req , res)=>{
+    console.log(req.params.result)
+    let id = req.params.result
+    let idformat = id.toString()
+    result.map((resu)=>{
+        if(idformat == resu.id){
+            res.send(resu)
+          
+            
+        }
+        
+    })
+} )
